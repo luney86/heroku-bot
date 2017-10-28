b66a9e0cc1858332d7c43a0437241ad18a0f352a
Hyunsoo Cho <luney86@gmail.com>
2017-10-28 14:06:49 -0400

    Update ChatterBot.py

--- ChatterBot.py
+++ ChatterBot.py
@@ -1,7 +1,7 @@
 # Dependencies
 import tweepy
-import json
 import time
+import json
 
 # Twitter API Keys
 consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
@@ -9,11 +9,25 @@ consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
 access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
 access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
 
-# Setup Tweepy API Authentication
+# Target Term
+target_term = "@Nationals"
+
+# Twitter Credentials
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
+# Search for all tweets
+public_tweets = api.search(target_term, count=5, result_type="recent")
 
-# Create a function that tweets
-# CODE GOES HERE
+# Loop through all public_tweets
+for tweet in public_tweets["statuses"]:
+
+    # Get ID and Author of most recent tweet directed to me
+    tweet_id = tweet["id"]
+    tweet_author = tweet["user"]["screen_name"]
+    tweet_text = tweet["text"]
+
+    # Print Tweet Text and Tweet Author
+    print(tweet_text)
+    print(tweet_author)
